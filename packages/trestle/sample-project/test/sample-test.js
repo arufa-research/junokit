2ae8ec3a20272ed6099605938436b94c35b8ddff
adarshsofitech <itsadarshkumar@gmail.com>
2022-05-07 15:33:59 +0530

    added test

--- packages/trestle/sample-project/test/sample-test.js
+++ packages/trestle/sample-project/test/sample-test.js
@@ -10,25 +10,53 @@ describe("erc-20", () => {
     const other = getAccountByName("account_0");
     const contract = new Contract("cw_erc20");
     await contract.parseSchema();
+    const deploy_response = await contract.deploy(
+    contract_owner,
+    { // custom fees
+      amount: [{ amount: "750000", denom: "ujunox" }],
+      gas: "3000000",
+    }
+  );
+  console.log(deploy_response)
 
     return { contract_owner, other, contract };
   }
 
   it("deploy and init", async () => {
+    const contract_info = await contract.instantiate(
+    {
+      "name": "ERC", "symbol": "ER", "decimals": 10,
+      "initial_balances": [{
+        "address": contract_owner.account.address,
+        "amount": "100000000"
+      }]
+    }, "deploy test", contract_owner);
+  console.log(contract_info);
+  });
+
+  it("transfer and query balance", async () => {
     const { contract_owner, other, contract } = await setup();
     const deploy_response = await contract.deploy(contract_owner);
     console.log(deploy_response);
 
     const contract_info = await contract.instantiate(
-      {
-        "decimals": 6,
-        "name": "SampleSnip",
-        "prng_seed": "YWE",
-        "symbol": "SMPL"
-      },
-      "deploy test",
-      contract_owner
-    );
-    console.log(contract_info);
+    {
+      "name": "ERC", "symbol": "ER", "decimals": 10,
+      "initial_balances": [{
+        "address": contract_owner.account.address,
+        "amount": "100000000"
+      }]
+    }, "deploy test", contract_owner);
+  console.log(contract_info);
+    let transfer_response = await contract.tx.transfer(
+    { account: contract_owner },
+    {
+      recipient: other.account.address,
+      amount: "50000000"
+    }
+  );
+  console.log(transfer_response);
+  let balance_after = await ;
+  await expect(contract.query.balance({ "address": contract_owner.account.address })).to.respondWith({"balance": "50000000"});
   });
 });

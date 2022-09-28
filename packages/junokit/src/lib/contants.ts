9870681401b778d4ebdd84f31945bb35b06bb1d3
udit-gulati <uditgulati6@gmail.com>
2022-09-28 21:28:28 +0530

    no need for amount in default fee as denom is network dependent

--- packages/junokit/src/lib/contants.ts
+++ packages/junokit/src/lib/contants.ts
@@ -2,22 +2,22 @@ export const JUNOKIT_NAME = "junokit";
 
 export const defaultFees = {
   upload: {
-    amount: [{ amount: "2000000", denom: "ujunox" }],
+    amount: [],
     gas: "20000000"
   },
 
   init: {
-    amount: [{ amount: "300000", denom: "ujunox" }],
+    amount: [],
     gas: "500000"
   },
 
   exec: {
-    amount: [{ amount: "300000", denom: "ujunox" }],
+    amount: [],
     gas: "500000"
   },
 
   send: {
-    amount: [{ amount: "80000", denom: "ujunox" }],
+    amount: [],
     gas: "80000"
   }
 };

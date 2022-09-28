fda44c08ef2c1c20a7d450e76cfa0fcae36d0a3c
udit-gulati <uditgulati6@gmail.com>
2022-09-28 21:23:28 +0530

    update junokit cfg for sample project

--- packages/junokit/sample-project/junokit.config.js
+++ packages/junokit/sample-project/junokit.config.js
@@ -1,8 +1,8 @@
-const accounts = [
+const testnet_accounts = [
   {
     name: 'account_0',
-    address: 'juno16g2rahf5846rxzp3fwlswy08fz8ccuwk03k57y',
-    mnemonic: 'clip hire initial neck maid actor venue client foam budget lock catalog sweet steak waste crater broccoli pipe steak sister coyote moment obvious choose'
+    address: 'juno1evpfprq0mre5n0zysj6cf74xl6psk96gus7dp5',
+    mnemonic: 'omit sphere nurse rib tribe suffer web account catch brain hybrid zero act gold coral shell voyage matter nose stick crucial fog judge text'
   },
   {
     name: 'account_1',
@@ -11,45 +11,49 @@ const accounts = [
   }
 ];
 
+const localnet_accounts = [
+  {
+    name: 'account_0',
+    address: 'juno16g2rahf5846rxzp3fwlswy08fz8ccuwk03k57y',
+    mnemonic: 'clip hire initial neck maid actor venue client foam budget lock catalog sweet steak waste crater broccoli pipe steak sister coyote moment obvious choose'
+  }
+];
+
+const mainnet_accounts = [
+];
+
 const networks = {
   localnet: {
     endpoint: 'http://localhost:26657/',
-    accounts: accounts
+    accounts: localnet_accounts
   },
   // uni-2
   testnet: {
-    endpoint: 'https://rpc.uni.juno.deuslabs.fi/',//https://lcd.uni.juno.deuslabs.fi/
-    chainId: 'uni-2',
+    endpoint: 'https://rpc.uni.juno.deuslabs.fi/',
+    chainId: 'uni-3',
     trustNode: true,
     keyringBackend: 'test',
-    accounts: accounts,
-    fees: {
-      upload: {
-          amount: [{ amount: "500000", denom: "ujunox" }],
-          gas: "4000000",
-      },
-      init: {
-          amount: [{ amount: "125000", denom: "ujunox" }],
-          gas: "500000",
-      },
-      exec: {
-        amount: [{ amount: "125000", denom: "ujunox" }],
-        gas: "500000",
-      },
-    }
+    accounts: testnet_accounts
   },
+  mainnet: {
+    endpoint: 'https://rpc-juno.itastakers.com/',
+    chainId: 'juno-1',
+    trustNode: true,
+    keyringBackend: 'test',
+    accounts: mainnet_accounts
+  }
 };
 
 module.exports = {
   networks: {
     default: networks.testnet,
     localnet: networks.localnet,
+    mainnet: networks.mainnet,
   },
   mocha: {
-    timeout: 60000,
-    require: ["ts-node/require"]
+    timeout: 60000
   },
   rust: {
-    version: "1.59.0",
+    version: "1.60.0",
   }
 };
